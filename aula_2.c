98359827f65b19090a4f47513e1346c00e502d43
UlissesAlbardeiro <ualbardeiro@gmail.com>
2023-08-27 09:50:16 -0300

    aula-2
    Função printf() e quebras de linha

---
+++ aula_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//caracter de escape para saltar uma linha -> \n
+
+int main(){
+
+//printf é uma função de saída
+    printf("\n--------------------------------------!\n");
+    printf("1 - Logar  2 - Cadastrar  3 - Imprimir");
+    printf("\n--------------------------------------!\n");
+
+    printf("\nValor retornado: %d", printf("Bom"));
+
+    return 0;
+}
