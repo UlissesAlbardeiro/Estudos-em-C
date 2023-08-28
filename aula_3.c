3b293b56a9443eb81217d130863a64c8d17cf6b2
UlissesAlbardeiro <ualbardeiro@gmail.com>
2023-08-28 20:26:26 -0300

    função getchar()

---
+++ aula_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Não usar a função system em linux e macos
+
+int main(){
+
+    printf("\nTeste no teminal");
+
+    printf("\nPressione qualquer tecla para finalizar");
+
+//usar a função getchar
+    getchar();
+
+    return 0;
+
+}
